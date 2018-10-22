f44bd6849741fa04eedd9c37caeab225b5e01685
Jeremy Cui <tsuijy@gmail.com>
2018-10-22 15:44:22 +0800

    Abstract the common list

---
+++ src/components/CommonList/index.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <div class="filter-container">
+      <slot name="filter-items"/>
+    </div>
+
+    <el-table
+      v-loading="listLoading"
+      :key="tableKey"
+      :data="list"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%;">
+
+      <slot name="columns" />
+
+      <el-table-column :label="$t('table.actions')" align="center" width="230" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">{{ $t('table.edit') }}</el-button>
+          <!-- <el-button size="mini" type="danger" @click="handleModifyStatus(scope.row,'deleted')">{{ $t('table.delete') }} -->
+          <!-- </el-button> -->
+
+          <slot name="extra-buttons"/>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div class="pagination-container">
+      <el-pagination v-show="total>0" :current-page="listQuery.page" :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit" :total="total" background layout="total, sizes, prev, pager, next, jumper" @size-change="handleSizeChange" @current-change="handleCurrentChange"/>
+    </div>
+
+    <slot name="form" />
+
+  </div>
+</template>
+
+<script>
+import waves from '@/directive/waves' // 水波纹指令
+
+export default {
+  name: 'CommonList',
+  filters: { },
+  directives: {
+    waves
+  },
+  props: {
+    listAction: {
+      type: Function,
+      default: () => new Promise()
+    }
+  },
+  data() {
+    return {
+      tableKey: 0,
+      list: null,
+      total: null,
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        limit: 20,
+        name: undefined,
+        sort: '+id'
+      }
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      this.listLoading = true
+      console.log(this.listAction)
+      this.listAction(this.listQuery).then(response => {
+        this.list = response.data.items
+        this.total = response.data.total
+
+        // Just to simulate the time of the request
+        setTimeout(() => {
+          this.listLoading = false
+        }, 1000)
+      })
+    },
+    handleFilter() {
+      this.listQuery.page = 1
+      this.getList()
+    },
+    handleSizeChange(val) {
+      this.listQuery.limit = val
+      this.getList()
+    },
+    handleCurrentChange(val) {
+      this.listQuery.page = val
+      this.getList()
+    },
+    // handleCreate() {
+    //   this.$refs['userForm'].resetTemp()
+    //   this.userFormVisible = true
+
+    //   this.$nextTick(() => {
+    //     // this.$refs['dataForm'].clearValidate()
+    //   })
+    // },
+    handleUpdate(row) {
+      // const user = Object.assign({}, row)
+
+      // this.formStatus = 'update'
+      // this.userFormVisible = true
+      // this.$refs['userForm'].temp = user
+
+      // this.$nextTick(() => {
+      //   // this.$refs['dataForm'].clearValidate()
+      // })
+    }
+    // handleDelete(row) {
+    //   this.$notify({
+    //     title: '成功',
+    //     message: '删除成功',
+    //     type: 'success',
+    //     duration: 2000
+    //   })
+    //   const index = this.list.indexOf(row)
+    //   this.list.splice(index, 1)
+    // }
+  }
+}
+</script>
--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -1,158 +1,95 @@
 <template>
   <div class="app-container">
-    <div class="filter-container">
-      <el-input v-model="listQuery.title" placeholder="名称" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter"/>
-      <el-select v-model="listQuery.sort" style="width: 140px" class="filter-item" @change="handleFilter">
-        <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key"/>
-      </el-select>
-      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">{{ $t('table.search') }}</el-button>
-      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">{{ $t('table.add') }}</el-button>
-    </div>
-
-    <el-table
-      v-loading="listLoading"
-      :key="tableKey"
-      :data="list"
-      border
-      fit
-      highlight-current-row
-      style="width: 100%;">
-      <el-table-column :label="$t('table.id')" align="center" width="65">
-        <template slot-scope="scope">
-          <span>{{ scope.row.id }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="创建时间" width="150px" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.createdTime | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="名称" min-width="110px">
-        <template slot-scope="scope">
-          <span class="link-type" @click="handleUpdate(scope.row)">{{ scope.row.name }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="邮箱" width="150px" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.email }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.actions')" align="center" width="230" class-name="small-padding fixed-width">
-        <template slot-scope="scope">
-          <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">{{ $t('table.edit') }}</el-button>
-          <el-button size="mini" type="danger" @click="handleModifyStatus(scope.row,'deleted')">{{ $t('table.delete') }}
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-
-    <div class="pagination-container">
-      <el-pagination v-show="total>0" :current-page="listQuery.page" :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit" :total="total" background layout="total, sizes, prev, pager, next, jumper" @size-change="handleSizeChange" @current-change="handleCurrentChange"/>
-    </div>
-
-    <user-form ref="userForm" :show.sync="userFormVisible" :status="formStatus" title="test" @createdUser="handleCreatedUser" @updatedUser="handleUpdatedUser" />
-
+    <common-list :list-action="listUserAction">
+      <template slot="columns">
+        <el-table-column :label="$t('table.id')" align="center" width="65">
+          <template slot-scope="scope">
+            <span>{{ scope.row.id }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="名称" min-width="110px">
+          <template slot-scope="scope">
+            <!-- <span class="link-type" @click="handleUpdate(scope.row)">{{ scope.row.name }}</span> -->
+            <span>{{ scope.row.name }}</span>
+          </template>
+        </el-table-column>
+      </template>
+    </common-list>
   </div>
 </template>
 
 <script>
 import { fetchList } from '@/api/user'
-import waves from '@/directive/waves' // 水波纹指令
 import UserForm from './components/form'
+import CommonList from '@/components/CommonList'
 
 export default {
   name: 'UserIndex',
-  components: { UserForm },
-  directives: {
-    waves
-  },
-  filters: { },
+  components: { CommonList, UserForm },
   data() {
     return {
       tableKey: 0,
       list: null,
       total: null,
-      listLoading: true,
-      userFormVisible: false,
-      formStatus: 'create',
-      listQuery: {
-        page: 1,
-        limit: 20,
-        name: undefined,
-        sort: '+id'
-      },
-      sortOptions: [{ label: 'ID Ascending', key: '+id' }, { label: 'ID Descending', key: '-id' }]
+      listUserAction: fetchList
+      // userFormVisible: false,
+      // formStatus: 'create',
+      // listQuery: {
+      //   page: 1,
+      //   limit: 20,
+      //   name: undefined,
+      //   sort: '+id'
+      // }
     }
-  },
-  created() {
-    this.getList()
   },
   methods: {
-    getList() {
-      this.listLoading = true
-      fetchList(this.listQuery).then(response => {
-        this.list = response.data.items
-        this.total = response.data.total
+    // fetchUserList() {
+    //   fetchList().then(response => {
+    //     this.list = response.data.items
+    //     this.total = response.data.total
+    //     setTimeout(() => { this.listLoading = false }, 1.5 * 1000) })
+    // }
+    // handleCreatedUser(addedUser) {
+    //   this.list.unshift(addedUser)
+    // },
+    // handleUpdatedUser(user) {
+    //   for (const v of this.list) {
+    //     if (v.id === user.id) {
+    //       const index = this.list.indexOf(v)
+    //       this.list.splice(index, 1, user)
+    //       break
+    //     }
+    //   }
+    // },
+    // handleCreate() {
+    //   this.$refs['userForm'].resetTemp()
+    //   this.userFormVisible = true
 
-        // Just to simulate the time of the request
-        setTimeout(() => {
-          this.listLoading = false
-        }, 1.5 * 1000)
-      })
-    },
-    handleFilter() {
-      this.listQuery.page = 1
-      this.getList()
-    },
-    handleSizeChange(val) {
-      this.listQuery.limit = val
-      this.getList()
-    },
-    handleCurrentChange(val) {
-      this.listQuery.page = val
-      this.getList()
-    },
-    handleCreatedUser(addedUser) {
-      this.list.unshift(addedUser)
-    },
-    handleUpdatedUser(user) {
-      for (const v of this.list) {
-        if (v.id === user.id) {
-          const index = this.list.indexOf(v)
-          this.list.splice(index, 1, user)
-          break
-        }
-      }
-    },
-    handleCreate() {
-      this.$refs['userForm'].resetTemp()
-      this.userFormVisible = true
+    //   this.$nextTick(() => {
+    //     // this.$refs['dataForm'].clearValidate()
+    //   })
+    // },
+    // handleUpdate(row) {
+    //   const user = Object.assign({}, row)
 
-      this.$nextTick(() => {
-        // this.$refs['dataForm'].clearValidate()
-      })
-    },
-    handleUpdate(row) {
-      const user = Object.assign({}, row)
+    //   this.formStatus = 'update'
+    //   this.userFormVisible = true
+    //   this.$refs['userForm'].temp = user
 
-      this.formStatus = 'update'
-      this.userFormVisible = true
-      this.$refs['userForm'].temp = user
-
-      this.$nextTick(() => {
-        // this.$refs['dataForm'].clearValidate()
-      })
-    },
-    handleDelete(row) {
-      this.$notify({
-        title: '成功',
-        message: '删除成功',
-        type: 'success',
-        duration: 2000
-      })
-      const index = this.list.indexOf(row)
-      this.list.splice(index, 1)
-    }
+    //   this.$nextTick(() => {
+    //     // this.$refs['dataForm'].clearValidate()
+    //   })
+    // },
+    // handleDelete(row) {
+    //   this.$notify({
+    //     title: '成功',
+    //     message: '删除成功',
+    //     type: 'success',
+    //     duration: 2000
+    //   })
+    //   const index = this.list.indexOf(row)
+    //   this.list.splice(index, 1)
+    // }
   }
 }
 </script>
